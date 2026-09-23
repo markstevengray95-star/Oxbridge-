cec81c6dc319de2b711465f8396d369ee44a34f3
markstevengray95-star <markstevengray95@gmail.com>
2026-09-23 13:19:31 +0100

    Cache Interview Hub and panel routes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='oxbridge-tutor-v6';
-const SHELL=['/','/interview-room','/ai-interview','/live-interview','/advanced-practice','/manifest.webmanifest','/favicon.svg'];
+const CACHE='oxbridge-tutor-v7';
+const SHELL=['/','/interviews','/interview-room','/ai-interview','/live-interview','/panel-interview','/advanced-practice','/manifest.webmanifest','/favicon.svg'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
