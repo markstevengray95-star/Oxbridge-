a18af96d2730f9ec125e4d704432109a92d58581
markstevengray95-star <markstevengray95@gmail.com>
2026-09-26 20:30:16 +0100

    Mount UCAT calculator in full papers

--- app/full-papers/page.tsx
+++ app/full-papers/page.tsx
@@ -1,5 +1,9 @@
 import FullPaperCentre from "@/components/full-paper-centre"
+import { UcatBasicCalculator } from "@/components/ucat-basic-calculator"
 
 export default function FullPapersPage() {
-  return <FullPaperCentre />
+  return <>
+    <FullPaperCentre />
+    <UcatBasicCalculator autoDetect />
+  </>
 }
