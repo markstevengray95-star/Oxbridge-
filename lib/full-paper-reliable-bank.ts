86b6276fae2c1312f1511dd683e9b2c2d99d6ff9
markstevengray95-star <markstevengray95@gmail.com>
2026-09-26 13:47:02 +0100

    Add resilient ESAT breadth reserves

--- lib/full-paper-reliable-bank.ts
+++ lib/full-paper-reliable-bank.ts
@@ -3,6 +3,7 @@ import { reliabilityUpgradeQuestionBank } from "@/lib/question-bank-reliability-
 import { ucatQrReliabilityUpgradeBank } from "@/lib/ucat-qr-reliability-upgrades"
 import { tmuaSpecificationExpansionBank } from "@/lib/tmua-spec-expansion"
 import { esatSpecificationExpansionBank } from "@/lib/esat-spec-expansion"
+import { esatSpecificationReserveBank } from "@/lib/esat-spec-reserve"
 import { auditQuestionReliability, repairQuestionReliability, reliabilityScore } from "@/lib/question-reliability"
 import { repairSemanticAnswerCues } from "@/lib/question-integrity-repair"
 
@@ -20,7 +21,8 @@ const rawUpgrades = [...primaryUpgrades, ...ucatQrReliabilityUpgradeBank]
 const upgradedRepaired = rawUpgrades.map(repair)
 const tmuaSpecRepaired = tmuaSpecificationExpansionBank.map(repair)
 const esatSpecRepaired = esatSpecificationExpansionBank.map(repair)
-const repaired = [...tmuaSpecRepaired, ...esatSpecRepaired, ...upgradedRepaired, ...originalRepaired]
+const esatSpecReserveRepaired = esatSpecificationReserveBank.map(repair)
+const repaired = [...tmuaSpecRepaired, ...esatSpecRepaired, ...esatSpecReserveRepaired, ...upgradedRepaired, ...originalRepaired]
 
 export const reliableFullPaperQuestionBank = repaired.filter(question =>
   auditQuestionReliability(question).blocking.length === 0,
@@ -31,6 +33,7 @@ export const reliableFullPaperQuestionBankStats = {
   upgraded: upgradedRepaired.length,
   tmuaSpecExpansion: tmuaSpecRepaired.length,
   esatSpecExpansion: esatSpecRepaired.length,
+  esatSpecReserve: esatSpecReserveRepaired.length,
   reserve: originalRepaired.length,
   repairedUpgradeOptions: rawUpgrades.filter((question, index) =>
     JSON.stringify(question.options) !== JSON.stringify(upgradedRepaired[index]?.options),
